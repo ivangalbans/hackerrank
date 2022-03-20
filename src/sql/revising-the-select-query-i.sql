5985cfe7c187fc16d9d8057813c997028f2e0ad1
Ivan Galban <ivan.galban.smith@gmail.com>
2022-03-20 16:17:55 -0600

    Solve sql

--- src/sql/revising-the-select-query-i.sql
+++ src/sql/revising-the-select-query-i.sql
@@ -1,3 +1,3 @@
 --https://www.hackerrank.com/challenges/revising-the-select-query/problem
 
-select * from CITY where COUNTRYCODE="USA" && POPULATION > 100000
+SELECT * FROM CITY WHERE COUNTRYCODE="USA" && POPULATION > 100000;
